2a005f5e5c4328a5767e1849e276a85d69c678bb
chean5o <81213566+chean5o@users.noreply.github.com>
2021-04-09 00:36:30 +0900

    Create 1076-저항.py
    add week 1 codes

---
+++ Chaeeun/1076-저항.py
@@ -0,0 +1,7 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+first = color.index(input())
+second = color.index(input())
+third = color.index(input())
+
+result = int(str(first)+ str(second)) * (10 ** third)
+print(result)

4a00982ad31620d5207423c16042fe8a252329b8
chean5o <81213566+chean5o@users.noreply.github.com>
2021-04-30 19:24:20 +0900

    Delete Chaeeun directory

--- Chaeeun/1076-저항.py
+++
@@ -1,8 +0,0 @@
-color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
-first = color.index(input())
-second = color.index(input())
-third = color.index(input())
-
-result = int(str(first)+ str(second)) * (10 ** third)
-print(result)
-
